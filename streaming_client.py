4285e28c84802e1f7cc6263866ba56f25b59faeb
Wenqing Zong <wenqing.zong98@gmail.com>
2024-04-03 17:18:32 +0100

    Match 0.1.6 version

--- streaming_client.py
+++ streaming_client.py
@@ -24,16 +24,18 @@ def handle_args():
     parser.add_argument('--base64', action='store_true', help='Whether to transfer base64 encoded audio or just a binary stream')
     parser.add_argument('--keep-connection', action='store_true', help='Whether to keep ws connected after inference finished')
     parser.add_argument('--auth-token', type=str, required=True, help='Your Emotech authorization token, include it for every request')
+    parser.add_argument('--channels', type=int, choices=[1, 2], default=1, help='Number of channels to send to the server')
 
     return parser.parse_args()
 
 
-def asr_start_message(request_id: str, sample_rate: int, encoding: str, keep_connection: bool) -> str:
+def asr_start_message(request_id: str, sample_rate: int, encoding: str, keep_connection: bool, channels: int) -> str:
     start_message = {
         'request':'start',
         'params':{
-            'encoding':encoding,
-            'sample_rate':sample_rate
+            'encoding': encoding,
+            'sample_rate': sample_rate,
+            'channel_count': channels,
         },
         'config':{
             'keep_connection': keep_connection
@@ -62,7 +64,7 @@ def asr_stop_message() -> str:
     return json.dumps(stop_message)
 
 
-async def record_and_send(ws, sample_rate: int, encoding: str, base64: bool, request_id: str, finish_event: asyncio.Event) -> None:
+async def record_and_send(ws, sample_rate: int, encoding: str, base64: bool, request_id: str, finish_event: asyncio.Event, channels: int) -> None:
     if encoding == 's16':
         audio_format = pyaudio.paInt16
     elif encoding == 's32':
@@ -77,7 +79,7 @@ async def record_and_send(ws, sample_rate: int, encoding: str, base64: bool, req
 
     audio = pyaudio.PyAudio()
     frames_per_buffer = 1600
-    stream = audio.open(format=audio_format, channels=1, rate=sample_rate, input=True, frames_per_buffer=frames_per_buffer)
+    stream = audio.open(format=audio_format, channels=channels, rate=sample_rate, input=True, frames_per_buffer=frames_per_buffer)
 
     audio_buffer = []
     try:
@@ -89,7 +91,6 @@ async def record_and_send(ws, sample_rate: int, encoding: str, base64: bool, req
                 await ws.send(asr_audio_message(data))
             else:
                 await ws.send(data)
-                # print("Data sent,", datetime.now())
 
             # Yield control to allow other tasks to run.
             await asyncio.sleep(0)
@@ -130,7 +131,7 @@ async def main() -> None:
     args = handle_args()
     request_id = args.request_id if args.request_id != '' else str(uuid.uuid4())
 
-    start_message = asr_start_message(request_id, args.sample_rate, args.encoding, args.keep_connection)
+    start_message = asr_start_message(request_id, args.sample_rate, args.encoding, args.keep_connection, args.channels)
 
     finish_event = asyncio.Event()
 
@@ -140,13 +141,15 @@ async def main() -> None:
 
     if args.language == 'auto':
         url = 'wss://asr-whisper-http.api.emotechlab.com/ws/assess'
+        # url = 'ws://goliath.emotechlab.com:5555/ws/assess'
     else:
         url = 'wss://asr-whisper-http.api.emotechlab.com/ws/' + args.language + '/assess'
+        # url = 'ws://goliath.emotechlab.com:5555/ws/' + args.language + '/assess'
 
     async with websockets.connect(url, extra_headers=headers) as ws:
         await ws.send(start_message)
         receive_task = receive_responses(ws, finish_event)
-        send_task = record_and_send(ws, args.sample_rate, args.encoding, args.base64, request_id, finish_event)
+        send_task = record_and_send(ws, args.sample_rate, args.encoding, args.base64, request_id, finish_event, args.channels)
 
         # Run both tasks concurrently
         await asyncio.gather(send_task, receive_task)
@@ -157,5 +160,13 @@ if __name__ == '__main__':
         asyncio.run(main())
     except ConnectionClosedOK:
         pass
-    except Exception as e:
+    except OSError as e:
         print(e, file=stderr)
+        print("Below is a summary of your input devices:", file=stderr)
+        p = pyaudio.PyAudio()
+        for i in range(p.get_device_count()):
+            dev = p.get_device_info_by_index(i)
+            print(f"{i}. {dev['name']} - Max Input Channels: {dev['maxInputChannels']}", file=stderr)
+        p.terminate()
+    except Exception as e:
+        print(type(e), e, file=stderr)
